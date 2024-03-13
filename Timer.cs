6ae46c6c5833133c4cf7a3f3f636990e425ac20a
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2024-03-13 02:24:33 +0000

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -2,32 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+namespace GRP.Unity
 {
-    enum Count { Down = -1, Up = 1 }
-
-    [SerializeField] Count _count = Count.Up;
-    [SerializeField, Min(0)] float _startSecs = 0f;
-    [SerializeField] bool _playOnStart = true;
-
-    public float Secs      { get; private set; } = 0f;
-    public bool  IsRunning { get; private set; } 
-
-    void Start()
+    public class Timer : MonoBehaviour
     {
-        Secs      = _startSecs;
-        IsRunning = _playOnStart;
+        enum Count { Down = -1, Up = 1 }
+
+        [SerializeField] Count _count = Count.Up;
+        [SerializeField, Min(0)] float _startSecs = 0f;
+        [SerializeField] bool _playOnStart = true;
+
+        public float Secs      { get; private set; } = 0f;
+        public bool  IsRunning { get; private set; } 
+
+        void Start()
+        {
+            Secs      = _startSecs;
+            IsRunning = _playOnStart;
+        }
+
+        void Update()
+        {
+            if (IsRunning) 
+                Secs += Time.deltaTime * (float)_count;
+        }
+
+        public void Play() => IsRunning = true;
+        public void Stop() => IsRunning = false;
+
+        public string FmtMinutes()
+            => $"{Mathf.FloorToInt(Secs / 60):00}:{Mathf.FloorToInt(Secs % 60):00}";
     }
-
-    void Update()
-    {
-        if (IsRunning) 
-            Secs += Time.deltaTime * (float)_count;
-    }
-
-    public void Play() => IsRunning = true;
-    public void Stop() => IsRunning = false;
-
-    public string FmtMinutes()
-        => $"{Mathf.FloorToInt(Secs / 60):00}:{Mathf.FloorToInt(Secs % 60):00}";
 }
